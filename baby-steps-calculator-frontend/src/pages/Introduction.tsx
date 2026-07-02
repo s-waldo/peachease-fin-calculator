a5baea73efa9c7d98b491475a5b3012fbd705cfb
Stephen Waldo <stephen.waldo.94@gmail.com>
2026-07-02 13:23:51 -0500

    feat: restructure Introduction component layout for improved readability and user experience

--- baby-steps-calculator-frontend/src/pages/Introduction.tsx
+++ baby-steps-calculator-frontend/src/pages/Introduction.tsx
@@ -1,20 +1,30 @@
 import { useGlobalStore } from "../utils/state/globalState"
 
 export default function Introduction() {
-  const nextStep = useGlobalStore(state => state.nextStep)
+  const nextStep = useGlobalStore((state) => state.nextStep)
   return (
-    <>
-      <h2 className="text-2xl font-bold">Welcome to Peachy!</h2>
-      <p>Get started today to project your financial future.</p>
-      <h3 className="italic">How does it work?</h3>
-      <p>
-        Answer a few questions to get your financial freedom date in accordance
-        to the baby steps plan. Once calculated, you'll be able to come back in
-        the future to track your progress!
-      </p>
+    <div className="grid lg:grid-cols-2 grid-rows-1 min-h-dvh p-10">
+      <div className="flex flex-col items-center justify-center gap-10">
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">Welcome to Peachy!</h2>
+          <p>Get started today to project your financial future.</p>
+        </div>
+        <div>
+          <h3 className="italic">How does it work?</h3>
+          <p>
+            Answer a few questions to get your financial freedom date in
+            accordance to the baby steps plan. Once calculated, you'll be able to
+            come back in the future to track your progress!
+          </p>
+        </div>
+      </div>
 
-      <p>Ready to go?</p>
-      <button className="btn btn-primary" onClick={nextStep}>Start</button>
-    </>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <p>Ready to start?</p>
+        <button className="btn btn-primary w-1/2" onClick={nextStep}>
+          Take the quiz
+        </button>
+      </div>
+    </div>
   )
 }
